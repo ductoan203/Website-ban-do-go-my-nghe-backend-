843266fa0d946461818e31ab0c60c986f2712eba
ductoan203 <ninhductoan28112003@gmail.com>
2025-06-27 13:25:10 +0700

    ok

--- src/main/java/com/example/doan/DoanApplication.java
+++ src/main/java/com/example/doan/DoanApplication.java
@@ -2,8 +2,11 @@ package com.example.doan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
 public class DoanApplication {
 
 	public static void main(String[] args) {
